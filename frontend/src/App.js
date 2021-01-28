b679ecfd92d601c08aba4ca12eae828705631311
unknown <miloszz87@gmail.com>
2021-01-28 10:49:56 -0500

    created home screen and product card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
       <main className="py-3">
           <Container>
             <h1>Welcome to EngraVape</h1>
+            <HomeScreen />
           </Container>
       </main>
     <Footer />
