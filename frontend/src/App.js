f0cc268897ddb229813f29eaab6278d0e49dfd82
unknown <miloszz87@gmail.com>
2021-02-05 19:20:50 -0500

    displaying items in cart with subtotal, added quantity and price change, added remove from cart action and reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ const App = () => {
       <Header />
         <main className="py-3">
             <Container>
-              <h1>Welcome to EngraVape</h1>
               <Route exact path='/' component={HomeScreen} />
               <Route path='/product/:id' component={ProductScreen} />
               <Route path='/cart/:id?' component={CartScreen} />
