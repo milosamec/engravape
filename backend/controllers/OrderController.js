9fe381d4c570af79937c9b67817c54c2619b151a
unknown <miloszz87@gmail.com>
2021-02-09 20:58:33 -0500

    displaying users orders, clearing state on logout

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -66,4 +66,16 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     }
 })
 
-export {addOrderItems, getOrderById, updateOrderToPaid}
+// @ desc Get logged in user orders
+// @ route GET /api/orders/myorders
+// @ access Private
+
+const getMyOrders = asyncHandler(async (req, res) => {
+    
+    const orders = await Order.find({ user: req.user._id })
+
+    console.log(orders)
+    res.json(orders)
+})
+
+export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders}
