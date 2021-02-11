cd3072a0afbf0a65c66d540a41d974999ad39e33
unknown <miloszz87@gmail.com>
2021-02-11 14:19:13 -0500

    listing all products, added delete functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,6 +11,9 @@ const getProducts = asyncHandler(async (req, res) => {
 })
 
 
+// @ desc Get a product by id
+// @ route GET /api/products/:id
+// @ access Public
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
@@ -22,4 +25,22 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 })
 
-export {getProducts, getProductById}
+// @ desc Delete a product
+// @ route DELETE /api/products/:id
+// @ access Private/Admin
+
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) { 
+        await product.remove()
+        res.json({ message: 'Product removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+
+
+export {getProducts, getProductById, deleteProduct}
