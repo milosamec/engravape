e7b0cdc347585996211b52585c343960afcbca33
unknown <miloszz87@gmail.com>
2021-02-17 11:05:26 -0500

    created product carousel, added morgan

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,8 +6,21 @@ import Product from '../models/productModel.js'
 // @ route GET /api/products
 // @ access Public
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
+    const pageSize = 10
+    const page = Number(req.query.pageNumber) || 1
+
+    const keyword = req.query.keyword ? {
+        name: {
+            $regex: req.query.keyword,
+            $options: 'i'
+        }
+    } : {}
+
+    const count = await Product.countDocuments({ ...keyword })
+    const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page -1))
+
+
+    res.json({ products, page, pages: Math.ceil(count / pageSize)})
 })
 
 
@@ -134,5 +147,17 @@ const createProductReview = asyncHandler(async (req, res) => {
 
 })
 
+// @ desc Get top rated products
+// @ route PUT /api/products/top
+// @ access Public
 
-export {getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview}
+const getTopProducts = asyncHandler(async (req, res) => {
+
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+  res.json(products)
+
+})
+
+
+export {getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview, getTopProducts}
