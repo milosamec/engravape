a7ac9649951d728585bb38de629227a44d8d5056
Milo Samec <43768673+milosamec@users.noreply.github.com>
2021-05-13 10:48:31 -0400

    Update userModel.js
    comment

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,8 +26,12 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
 
+// Before we have password, we want to encrypt the password
 userSchema.pre('save', async function (next) {
 
+    // This check is part of user update profile where users can update password
+    // So if we only update our name or email, we don't want this to run because it will create a new hash
+    // And we won't be able to log in
     if(!this.isModified('password')) {
         next()
     }
