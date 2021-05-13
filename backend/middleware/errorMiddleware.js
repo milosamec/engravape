8da5eb5c1b906840beb6643a7dcd778fda1dd828
Milo Samec <43768673+milosamec@users.noreply.github.com>
2021-05-13 10:15:32 -0400

    Update errorMiddleware.js
    Comments

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,7 @@
 const notFound = (err, req, res, next) => {
+    // Check for status code and set accordingly
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    //respong with statuscode and error message
     res.status(statusCode)
     res.json({
         message: err.message,
@@ -7,9 +9,13 @@ const notFound = (err, req, res, next) => {
     })
 }
 
+// Error handler for routes that don't exist
 const errorHandler = (req, res, next) => {
+    // Create an error var if route is not found
     const error = new Error(`Not Found - ${req.originalUrl}`)
+    // Respond with 404
     res.status(404)
+    // Procieed with request by passing the error we created
     next(error)
 }
 
